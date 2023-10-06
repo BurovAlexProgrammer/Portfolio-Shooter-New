8e131204b73f154af6dcf1002a0947ed19af86fb
Aleksei Burov <burovalexprogrammer@gmail.com>
2023-10-06 10:09:12 +0200

    Add test mesh and animations

---
+++ Assets/Scripts/Constants/AnimatorParams.cs
@@ -0,0 +1,8 @@
+namespace Game.Constants
+{
+    public class AnimatorParams
+    {
+        public const string IsWalking = "IsWalking";
+        public const string IsRunning = "IsRunning";
+    }
+}
