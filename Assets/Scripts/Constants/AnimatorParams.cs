57bd8308c00625c831df48b50b7ac90166cf6653
Aleksei Burov <burovalexprogrammer@gmail.com>
2023-10-09 06:40:16 +0200

    Clean project. Add simple animations

--- Assets/Scripts/Constants/AnimatorParams.cs
+++ Assets/Scripts/Constants/AnimatorParams.cs
@@ -6,6 +6,7 @@ namespace Game.Constants
     {
         private static int Hash(string paramName) => Animator.StringToHash(paramName);
         
+        public static readonly int IsTest = Hash("isTest");
         public static readonly int IsWalking = Hash("IsWalking");
         public static readonly int IsRunning = Hash("IsRunning");
         public static readonly int Velocity = Hash("f_Velocity");
